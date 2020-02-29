87344770f9576bed19b73d111dafefb4c63169e8
Stephanos Ioannidis <root@stephanos.io>
2020-02-29 21:11:34 +0900

    Move buildlocaltools :Build function to a separate file
    The `:Build` function, which is used to automate the SDK tool build
    process, was moved to a separate batch file, in order to support
    building on NT 5.1.
    
    For more details, refer to the issue #13.

--- buildlocaltools.cmd
+++ buildlocaltools.cmd
@@ -54,82 +54,82 @@ REM //
 echo.
 echo == Building local tools ... ==
 
-call :Build mkmsg sdktools\mkmsg bldtools\mkmsg.exe idw\mkmsg.exe
+call buildlocaltool.cmd mkmsg sdktools\mkmsg bldtools\mkmsg.exe idw\mkmsg.exe
 if errorlevel 1 goto Error
 
-call :Build nmake sdktools\nmake bldtools\nmake.exe mstools\nmake.exe
+call buildlocaltool.cmd nmake sdktools\nmake bldtools\nmake.exe mstools\nmake.exe
 if errorlevel 1 goto Error
 
-call :Build build sdktools\build idw\build.exe idw\build.exe
+call buildlocaltool.cmd build sdktools\build idw\build.exe idw\build.exe
 if errorlevel 1 goto Error
 
-call :Build debuggers#dbg-common sdktools\debuggers\dbg-common
+call buildlocaltool.cmd debuggers#dbg-common sdktools\debuggers\dbg-common
 if errorlevel 1 goto Error
 
-call :Build debuggers#minidump sdktools\debuggers\minidump
+call buildlocaltool.cmd debuggers#minidump sdktools\debuggers\minidump
 if errorlevel 1 goto Error
 
-call :Build debuggers#imagehlp#tools sdktools\debuggers\imagehlp\tools
+call buildlocaltool.cmd debuggers#imagehlp#tools sdktools\debuggers\imagehlp\tools
 if errorlevel 1 goto Error
 
-call :Build debuggers#imagehlp#dbghelp sdktools\debuggers\imagehlp\dbghelp
+call buildlocaltool.cmd debuggers#imagehlp#dbghelp sdktools\debuggers\imagehlp\dbghelp
 if errorlevel 1 goto Error
 
-call :Build debuggers#imagehlp#imagehlp sdktools\debuggers\imagehlp\imagehlp
+call buildlocaltool.cmd debuggers#imagehlp#imagehlp sdktools\debuggers\imagehlp\imagehlp
 if errorlevel 1 goto Error
 
-call :Build debuggers#imagehlp#binplace sdktools\debuggers\imagehlp\binplace idw\binplace.exe idw\binplace.exe
+call buildlocaltool.cmd debuggers#imagehlp#binplace sdktools\debuggers\imagehlp\binplace idw\binplace.exe idw\binplace.exe
 if errorlevel 1 goto Error
 
-call :Build debuggers#symchk#symcommon sdktools\debuggers\symchk\symcommon
+call buildlocaltool.cmd debuggers#symchk#symcommon sdktools\debuggers\symchk\symcommon
 if errorlevel 1 goto Error
 
-call :Build debuggers#symchk#symchk#dll sdktools\debuggers\symchk\symchk\dll dbg\files\bin\symbolcheck.dll idw\symbolcheck.dll
+call buildlocaltool.cmd debuggers#symchk#symchk#dll sdktools\debuggers\symchk\symchk\dll dbg\files\bin\symbolcheck.dll idw\symbolcheck.dll
 if errorlevel 1 goto Error
 
-call :Build debuggers#symchk#symchk#exe sdktools\debuggers\symchk\symchk\exe dbg\files\bin\symchk.exe idw\symchk.exe
+call buildlocaltool.cmd debuggers#symchk#symchk#exe sdktools\debuggers\symchk\symchk\exe dbg\files\bin\symchk.exe idw\symchk.exe
 if errorlevel 1 goto Error
 
-call :Build ztools sdktools\ztools
+call buildlocaltool.cmd ztools sdktools\ztools
 if errorlevel 1 goto Error
 
-call :Build alias sdktools\alias bldtools\alias.exe idw\alias.exe
+call buildlocaltool.cmd alias sdktools\alias bldtools\alias.exe idw\alias.exe
 if errorlevel 1 goto Error
 
-call :Build pcopy sdktools\pcopy bldtools\pcopy.exe idw\pcopy.exe
+call buildlocaltool.cmd pcopy sdktools\pcopy bldtools\pcopy.exe idw\pcopy.exe
 if errorlevel 1 goto Error
 
-call :Build rcdll sdktools\rcdll bldtools\rcdll.dll mstools\rcdll.dll
+call buildlocaltool.cmd rcdll sdktools\rcdll bldtools\rcdll.dll mstools\rcdll.dll
 if errorlevel 1 goto Error
 
-call :Build rc sdktools\rc bldtools\rc.exe mstools\rc.exe
+call buildlocaltool.cmd rc sdktools\rc bldtools\rc.exe mstools\rc.exe
 if errorlevel 1 goto Error
 
-call :Build mc sdktools\mc bldtools\mc.exe mstools\mc.exe
+call buildlocaltool.cmd mc sdktools\mc bldtools\mc.exe mstools\mc.exe
 if errorlevel 1 goto Error
 
-call :Build touch sdktools\touch bldtools\touch.exe idw\touch.exe
+call buildlocaltool.cmd touch sdktools\touch bldtools\touch.exe idw\touch.exe
 if errorlevel 1 goto Error
 
-call :Build m4 sdktools\m4 bldtools\m4.exe idw\m4.exe
+call buildlocaltool.cmd m4 sdktools\m4 bldtools\m4.exe idw\m4.exe
 if errorlevel 1 goto Error
 
-call :Build tracewpp sdktools\trace\tracewpp bldtools\tracewpp.exe idw\tracewpp.exe
+call buildlocaltool.cmd tracewpp sdktools\trace\tracewpp bldtools\tracewpp.exe idw\tracewpp.exe
 if errorlevel 1 goto Error
 
-call :Build hextract sdktools\hextract idw\hextract.exe idw\hextract.exe
+call buildlocaltool.cmd hextract sdktools\hextract idw\hextract.exe idw\hextract.exe
 if errorlevel 1 goto Error
 
-call :Build hsplit sdktools\hsplit idw\hsplit.exe idw\hsplit.exe
+call buildlocaltool.cmd hsplit sdktools\hsplit idw\hsplit.exe idw\hsplit.exe
 if errorlevel 1 goto Error
 
-call :Build wcshdr sdktools\wcshdr idw\wcshdr.exe idw\wcshdr.exe
+call buildlocaltool.cmd wcshdr sdktools\wcshdr idw\wcshdr.exe idw\wcshdr.exe
 if errorlevel 1 goto Error
 
-call :Build reloc sdktools\reloc bldtools\reloc.exe tools16\reloc.exe
+call buildlocaltool.cmd reloc sdktools\reloc bldtools\reloc.exe tools16\reloc.exe
 if errorlevel 1 goto Error
 
-call :Build nosrvbld sdktools\nosrvbld bldtools\nosrvbld.exe tools16\nosrvbld.exe
+call buildlocaltool.cmd nosrvbld sdktools\nosrvbld bldtools\nosrvbld.exe tools16\nosrvbld.exe
 if errorlevel 1 goto Error
 
 REM //
@@ -166,53 +166,5 @@ pause
 
 endlocal
 popd
-
-exit /b 0
-
-REM //
-REM // ** Build Function **
-REM //
-REM // %~1 = Tool name
-REM // %~2 = Relative source directory path under %NTROOT%
-REM // %~3 = Relative binary path under %NTTREE%
-REM // %~4 = Relative binary path under %BETOOLS%
-REM //
-
-:Build
-echo.
-echo [%~1]
-
-REM //
-REM // Verify that the source directory for the specified tool exists.
-REM //
-
-if not exist "%NTROOT%\%~2" (
-    echo %~1 does not exist in the local source tree. Skipping %~1 build.
-    exit /b
-)
-
-cd "%NTROOT%\%~2"
-
-REM //
-REM // Perform build.
-REM //
-
-build -c
-if errorlevel 1 (
-    echo Build command failed for %~1.
-    exit /b 1
-)
-
-REM //
-REM // Copy the built tool to the tools directory.
-REM //
-
-if not [%~3] equ [] (
-    copy /y "%NTTREE%\%~3" "%BETOOLS%\%~4"
-    if errorlevel 1 (
-        echo Failed to copy %~1 to tools directory.
-        exit /b 2
-    )
-)
 
 exit /b 0
